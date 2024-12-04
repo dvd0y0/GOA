4a532f9736d097a86c26d0b0306615e35723ce10
dvd0y0 <dvdmetskhovrishvili@gmail.com>
2024-12-04 21:43:29 +0400

    commit

---
+++ Level 16/classwork/Level 16.py
@@ -0,0 +1,25 @@
+# user_number = int(input("Enter user number: "))
+
+# even_numbers = [num for num in range(user_number + 1) if num % 2 == 0]
+
+# print("Even numbers from 0 to", user_number, ":", even_numbers)
+
+# count_even_numbers = len(even_numbers)
+# print("Number of even numbers:", count_even_numbers)
+
+
+
+# # 1:
+# print("\nExample 1: Numbers from 1 to 5")
+# for i in range(1, 6):
+# print(i) 
+
+# #2:
+# print("\nExample 2: Even numbers from 0 to 10")
+# for i in range(0, 11, 2):
+# print(i)
+
+# #3:
+# print("\nExample 3: Squares of numbers from 1 to 5")
+# for i in range(1, 6):
+# print(i ** 2)
---
+++ Level 16/homework/Level 16.py
@@ -0,0 +1,35 @@
+# #1
+# start = int(input("Please enter the start of the range: "))
+# end = int(input("Please enter the end of the range: "))
+# step = int(input("Please enter the step: "))
+
+# range_numbers = range(start, end, step)
+# print("Numbers created: ", list(range_numbers))
+
+# #2
+
+# start = int(input("Please enter the start of the range: "))
+# end = int(input("Please enter the end of the range: "))
+
+# even_numbers = [num for num in range(start, end + 1) if num % 2 == 0]
+# print("Even numbers in range: ", even_numbers)
+
+# #3
+
+# even_numbers = [num for num in range(11) if num % 2 == 0]
+# print("Even numbers from 0 to 10:", even_numbers)
+
+# count_even_numbers = len(even_numbers)
+# print("Number of even numbers:", count_even_numbers)
+
+# print("\nExample 1: Numbers from 1 to 5")
+# for i in range(1, 6):
+# print(i)
+
+# print("\nExample 2: Even numbers from 0 to 10")
+# for i in range(0, 11, 2):
+# print(i)
+
+# print("\nExample 3: Squares of numbers from 1 to 5")
+# for i in range(1, 6):
+# print(i ** 2) # Outputs 1 4 9 16 25
