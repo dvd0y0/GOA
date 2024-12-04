81f4f83d9b6084ba7b348a93d7f33ddf4dc2b99f
dvd0y0 <dvdmetskhovrishvili@gmail.com>
2024-12-04 23:11:38 +0400

    commit

---
+++ Level 11/classwork/Level 11.py
@@ -0,0 +1,13 @@
+# name = input("Enter your name:")
+
+# if name == "dato":
+#     print("True")
+# else:
+#     print("false")
+
+    #classwork 2
+
+#     age = int(input("enter your age: "))
+# my_age = 14
+
+# print(my_age == age or age > my_age)
