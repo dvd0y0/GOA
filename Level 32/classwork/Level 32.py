65cb3fa8abc8d4c21b28912e8d0beff262cf3531
dvd0y0 <dvdmetskhovrishvili@gmail.com>
2024-12-07 20:23:10 +0400

    Add files via upload

---
+++ Level 32/classwork/Level 32.py
@@ -0,0 +1,29 @@
+#1
+
+# def create_array(n):
+#     res=[]
+#     i=1
+#     while i<=n:
+#         res+=[i]
+#         i+=1
+
+#     return res 
+#2
+
+# def grow(arr)
+#     result = 1
+#     for num in arr:
+#         result +=num 
+#         return result
+#3
+
+# def fake_bin(x):
+#     bin = ''
+#     for i in x:
+#         if int(i) < 5:
+#             bin += '0'
+#         else:
+#             bin += '1'
+
+#     return bin
+
---
+++ Level 32/homework/Level 32.py
@@ -0,0 +1,38 @@
+#1
+
+# def human_years_cat_years_dog_years(human_years):
+#     cat_years = 0
+#     dog_years = 0
+
+#     if human_years == 1:
+#         cat_years = 15
+#         dog_years = 15
+#     elif human_years == 2:
+#         cat_years = 15 + 9
+#         dog_years = 15 + 9
+#     else:
+#         cat_years = 15 + 9 + (human_years - 2) * 4
+#         dog_years = 15 + 9 + (human_years - 2) * 5
+
+#     return [human_years, cat_years, dog_years]
+
+#2
+# def solution(string):
+#      return string[::-1]
+
+#3
+# def human_years_cat_years_dog_years(human_years):
+#     cat_years = 0
+#     dog_years = 0
+
+#     if human_years == 1:
+#         cat_years = 15
+#         dog_years = 15
+#     elif human_years == 2:
+#         cat_years = 15 + 9
+#         dog_years = 15 + 9
+#     else:
+#         cat_years = 15 + 9 + (human_years - 2) * 4
+#         dog_years = 15 + 9 + (human_years - 2) * 5
+
+#     return [human_years, cat_years, dog_years]
