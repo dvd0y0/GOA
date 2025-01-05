63383df5b5b176205ea0cdda7fc2429d77ccd926
dvd0y0 <dvdmetskhovrishvili@gmail.com>
2025-01-05 17:01:24 +0400

    commit

---
+++ Level 28/classwork/codewars.py
@@ -0,0 +1,25 @@
+# 1
+
+# def simple_multiplication(number):
+#     if number % 2 == 0:
+#         return number * 8 
+#     else:
+#         return number * 9
+
+# 2
+
+# def number_to_string(num):
+#     return str(num)
+
+# 3
+
+# def quarter_of(month):
+#     return (month - 1) // 3 + 1
+
+# 4
+
+# def check_alive(health):
+#     if health > 0:
+#         return True 
+#     else:
+#         return False
---
+++ Level 28/homework/codewars.py
@@ -0,0 +1,35 @@
+# 1
+
+# def get_grade(s1, s2, s3):
+#     average = (s1 + s2 + s3) / 3
+#     if 90 <= average <= 100:
+#         return "A"
+#     elif 80 <= average < 90:
+#         return "B"
+#     elif 70 <= average < 80:
+#         return "C"
+#     elif 60 <= average < 70:
+#         return "D"
+#     else:
+#         return "F"
+
+# 2
+
+# def multiply(a, b):
+#     return a * b
+
+# 3
+
+# def area_or_perimeter(l, w):
+#     if l == w:
+#         return l * w 
+#     else:
+#         return 2 * (l + w)
+
+# 4
+
+# def find_average(nums):
+#     if not nums:
+#         return 0  
+
+#     return sum(nums) / len(nums)
