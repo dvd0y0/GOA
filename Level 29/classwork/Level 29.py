d158bdc400746dd8843bdc15e9d6a6917c7fc113
dvd0y0 <dvdmetskhovrishvili@gmail.com>
2025-01-05 17:01:48 +0400

    commit

---
+++ Level 29/classwork/Level 29.py
@@ -0,0 +1,26 @@
+# №2
+
+# def sum(x):
+#     res = 0 
+#     for i in range(x):
+#         res+=i
+#     return res 
+
+# print(sum(4))
+
+# №3
+
+# def calc(x, y):
+#     return [x+y, x*y]
+
+# res = calc(3, 4)
+# print(res[1])
+
+# №5
+
+# def count_words(text):
+#     words = text.split()
+#     return len(words)
+
+# text = "ეს არის მაგალითი ტექსტი.მე ვარ ტექსტი"
+# print(count_words(text)) 
