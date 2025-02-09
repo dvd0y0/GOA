27c6e183c670616268533430f75f2fd51dc6b328
dvd0y0 <dvdmetskhovrishvili@gmail.com>
2025-02-09 23:05:57 +0400

    commit

---
+++ Level 42/classwork/codewars.py
@@ -0,0 +1,15 @@
+# 1
+
+# def check_alive(health):
+#     if health > 0:
+#         return True
+#     else:
+#         return False
+
+# 2
+
+# def grow(arr):
+#     num - 1
+#     for i in arr:
+#         num *=i
+#     return num 
---
+++ Level 42/homework/codewars.py
@@ -0,0 +1,24 @@
+# 1
+# function fakeBin(x) {
+#     return x.split('').map(digit => (digit < '5' ? '0' : '1')).join('');
+# }
+# 2
+# function solution(str) {
+#     return str.split('').reverse().join('');
+# }
+# 3
+# const areaOrPerimeter = function(l, w) {
+#     if (l === w) {
+#         return l * w;
+#     } else {
+#         return 2 * (l + w); 
+#     }
+# };
+# 4
+# function makeNegative(num) {
+#     return num > 0 ? -num : num;
+# }
+# 5
+# function opposite(number) {
+#     return -number;
+# }
