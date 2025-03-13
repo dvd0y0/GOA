8bffbffc886533b3749e0e2bb801730ee187a01d
dvd0y0 <dvdmetskhovrishvili@gmail.com>
2025-03-13 11:24:17 +0400

    commit

---
+++ Level 46/classwork/codewars.py
@@ -0,0 +1,24 @@
+# #1
+
+# def mouth_size(animal):
+#     if animal.lower() == "alligator":
+#         return "small"
+#     else:
+#         return "wide"
+
+# #2
+
+# def past(h, m, s):
+#     return (h * 3600000) + (m * 60000) + (s * 1000)
+
+# #3
+
+# def replace_exclamation(st):
+#     for vowel in 'aeiouAEIOU':
+#         st = st.replace(vowel, '!')
+#     return st
+
+# #4 
+
+# def set_alarm(employed, vacation):
+#     return employed and not vacation
---
+++ Level 46/homework/codewars.py
@@ -0,0 +1,44 @@
+# #1
+
+# def set_alarm(employed, vacation):
+#     return employed and not vacation
+
+# #2 
+
+# def update_light(current):
+#     if current == "green":
+#         return "yellow"
+#     elif current == "yellow":
+#         return "red"
+#     elif current == "red":
+#         return "green"
+
+# #3
+
+# def whatday(num):
+#     if num == 1:
+#         return "Sunday"
+#     elif num == 2:
+#         return "Monday"
+#     elif num == 3:
+#         return "Tuesday"
+#     elif num == 4:
+#         return "Wednesday"
+#     elif num == 5:
+#         return "Thursday"
+#     elif num == 6:
+#         return "Friday"
+#     elif num == 7:
+#         return "Saturday"
+#     else:
+#         return "Wrong, please enter a number between 1 and 7"
+
+# #4
+
+# def min_max(lst):
+#     return [min(lst), max(lst)]
+
+# #5
+
+# def sum_mix(arr):
+#     return sum([int(x) for x in arr])
