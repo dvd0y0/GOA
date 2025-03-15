150809905b211ae202f15ee8106fa7d16fba62ad
dvd0y0 <dvdmetskhovrishvili@gmail.com>
2025-03-15 19:50:54 +0400

    commit

---
+++ Level 47/classwork/Level 47.js
@@ -0,0 +1,25 @@
+// # 1 
+
+// let Balance = 450000
+
+// let withdraw = 450000
+
+// Balance -= 9000
+
+// withdraw += 9000
+
+// console.log(Balance)
+
+// console.log(withdraw)
+
+// # 2
+
+//  let name = prompt('შენი სახელი: ')
+
+//  let lastname = prompt('შენი გვარი: ')
+
+//  let age = prompt('შენი ასაკი: ')
+
+//  let placeofresidence = prompt('საცხოვრებელი ადგილი: ')
+
+//  console.log('გამარჯობა მე მქვია ' + name + ' და ჩემი გვარია ' + lastname + ' მე ვარ ' + age + ' წლის მე ვცხოვრობ ' + placeofresidence)
---
+++ Level 47/homework/Level 47.js
@@ -0,0 +1,41 @@
+// # 1
+
+//  let name = prompt('შენი სახელი: ')
+
+//  let lastname = prompt('შენი გვარი: ')
+
+//  let age = prompt('შენი ასაკი: ')
+
+//  let placeofresidence = prompt('საცხოვრებელი ადგილი: ')
+
+//  console.log('გამარჯობა მე მქვია ' + name + ' და ჩემი გვარია ' + lastname + ' მე ვარ ' + age + ' წლის მე ვცხოვრობ ' + placeofresidence)
+
+// # 2
+
+// let apple = parseInt(prompt("რამდენი ვაშლი გაქვს?:"));
+
+
+// let give = parseInt(prompt("რამდენი მიეცი მეგობარს?:"));
+
+// alert(apple - give)
+
+// # 3
+
+// let balanc = parseINT(prompt("შენი ბალანსი:"));
+
+// let Used = parseInt(prompt("შენი დანახარჯები:"));
+
+// alert(balanc + Used)
+
+// # 4
+
+//  let Total_coins = parseInt(prompt("რამდენი ვაშლი გაქვს?:"));
+
+//  let Used_coins = parseInt(prompt("რამდენი მიეცი მეგობარს?:"));
+
+//  alert(' წლის მე ვცხოვრობ ' + Total_coins)
+
+//  alert('გამარჯობა მე მქვია: ' Total_coins + Used_coins)
+
+//  alert('გამარჯობა მე მქვია: ' Total_coins - Used_coins)
+
