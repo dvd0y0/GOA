7cdd85d50ed698d8e257651da1acbbfcd7046988
dvd0y0 <dvdmetskhovrishvili@gmail.com>
2025-03-25 17:33:42 +0400

    commit

---
+++ Level 49/classwork/Level 49.js
@@ -0,0 +1,19 @@
+// #1
+// let Kiden = prompt('How old are you: ');
+
+// if(Kiden <= 19){
+//     alert('Take your KIden')
+// } else if(Kiden > 19){
+//     alert('you are too old')
+// }
+
+// #2
+// let color = prompt('your color: ');
+
+//  if('color == red'){
+//      alert('#1')
+// } else if('color == green'){
+//      alert('#2')
+// } else('color == bleack');{
+//     alert('3')
+// }
