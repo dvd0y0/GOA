9f97ab1fa7858f9e81d6455c6aa47bf5a48ab02f
dvd0y0 <dvdmetskhovrishvili@gmail.com>
2025-04-02 14:47:35 +0400

    commit

---
+++ Level 50/classwork/Level 50.js
@@ -0,0 +1,49 @@
+// 1
+
+// switch (number) {
+//     case '1':
+//         console.log('one');
+//         break;
+//     case '2':
+//         console.log('two');
+//         break;
+//     case '3':
+//         console.log('three');
+//         break;
+//     case '4':
+//         console.log('four');
+//         break;
+//     case '5':
+//         console.log('five');
+//         break;
+//     case '6':
+//         console.log('six');
+//         break;
+//     case '7':
+//         console.log('seven');
+//         break;
+//     case '8':
+//         console.log('eight');
+//         break;
+//     case '9':
+//         console.log('nine');
+//         break;
+// }
+
+// 2
+
+// let coffee = prompt('your coffee:')
+
+// switch(coffee){
+//     case '1':
+//         alert('Americano');
+//         break;
+//     case '2':
+//         alert('espresso');
+//         break;
+//     case '3':
+//         alert('cappuccino');
+//         break;
+//     default:
+//         alert('choose coffee')
+// }
---
+++ Level 50/homework/Level 50.js
@@ -0,0 +1,65 @@
+// # 1
+ 
+// let hour = prompt('enter your time: ');
+
+// switch (true) {
+//     case (hour >= 0 && hour <= 11):
+//         alert('am');
+//         break;
+//     case (hour >= 12 && hour <= 23):
+//         alert('pm');
+//         break;
+//     default:
+//         alert('Invalid hour');
+// }
+
+// # 2
+
+// let months = prompt('enter time of month:')
+
+// switch(months){
+//      case '1':
+//          alert('Monday');
+//          break;
+//      case '2':
+//          alert('Tuesday');
+//          break;
+//      case '3':
+//          alert('Wednesday');
+//          break;
+//      case '4':
+//          alert('Thursday');
+//          break;
+//      case '5':
+//          alert('Friday');
+//          break;
+//      case '6':
+//          alert('Saturday');
+//          break;
+//      case '7':
+//          alert('Sunday');
+//          break;
+//      default:
+//          alert('Wrong day')
+//  }
+
+// # 3
+
+//  let Weather = prompt('Enter the day:')
+
+//  switch(Weather){
+//       case '1':
+//           alert('Sunny');
+//           break;
+//       case '2':
+//           alert('rainy');
+//           break;
+//       case '3':
+//           alert('cloudy');
+//           break;
+//       case '4':
+//           alert('windy');
+//           break;
+//       default:
+//           alert('Not determined')
+//   }
