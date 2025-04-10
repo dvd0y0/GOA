0c2f8f26859cec3b2a9bc70219c7b86578a0012a
dvd0y0 <dvdmetskhovrishvili@gmail.com>
2025-04-10 15:35:15 +0400

    commit

---
+++ Level 51/classwork/Level 51.js
@@ -0,0 +1,24 @@
+// # 1
+
+// let res = (num == 1) ? 'One' :
+//           (num == 2) ? 'Two' :
+//           (num == 3) ? 'Three' :
+//           (num == 4) ? 'Four' :
+//           (num == 5) ? 'Five' :
+//           (num == 6) ? 'Six' :
+//           (num == 7) ? 'Seven' :
+//           (num == 8) ? 'Eight' :
+//           (num == 9) ? 'Nine' : 'enter a number'
+// return res
+
+// # 2
+
+// function getGrade(s1. s2, s3){
+//     let avg = (s1 + s2 + s3) / 3;
+//     return avg >= 90 ? 'A' :
+//            avg >= 80 ? 'B' :
+//            avg >= 70 ? 'C' :
+//            avg >= 60 ? 'D' : 'F'
+// }
+
+
---
+++ Level 51/homework/Level 51.js
@@ -0,0 +1,49 @@
+// # 1
+
+// let monthNum = prompt() 
+
+// let monthName = (monthNum === 1) ? "იანვარი" :
+//                 (monthNum === 2) ? "თებერვალი" :
+//                 (monthNum === 3) ? "მარტი" :
+//                 (monthNum === 4) ? "აპრილი" :
+//                 (monthNum === 5) ? "მაისი" :
+//                 (monthNum === 6) ? "ივნისი" :
+//                 (monthNum === 7) ? "ივლისი" :
+//                 (monthNum === 8) ? "აგვისტო" :
+//                 (monthNum === 9) ? "სექტემბერი" :
+//                 (monthNum === 10) ? "ოქტომბერი" :
+//                 (monthNum === 11) ? "ნოემბერი" :
+//                 (monthNum === 12) ? "დეკემბერი" : 'გთხოვთ, შეიყვანეთ რიცხვი 1-დან 12-მდე';
+
+// console.log(monthName);
+
+// # 2
+
+// let num1 = Number(prompt("შეიყვანეთ პირველი რიცხვი:"));
+// let num2 = Number(prompt("შეიყვანეთ მეორე რიცხვი:"));
+
+// let maxNumber = (num1 > num2) ? num1 : num2;
+
+// console.log(maxNumber);
+
+// # 3
+
+
+// let password = prompt("შეიყვანეთ პაროლი:");
+
+
+// let message = (password.length >= 8) ? "პაროლი საკმარისად გრძელია" : "პაროლი ძალიან მოკლეა";
+
+
+// console.log(message);
+
+// # 4
+
+// let quantity = parseInt(prompt("შეიყვანეთ შეძენილი საქონლის რაოდენობა:"));
+
+
+// let discount = (quantity >= 10) ? 10 : 0;
+
+
+// console.log(discount);
+
