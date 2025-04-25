95e8234d6d8f538c58c1a7d67f1f00dc19f7e956
dvd0y0 <dvdmetskhovrishvili@gmail.com>
2025-04-25 21:02:30 +0400

    commit

---
+++ Level 53/classwork/Level 53.js
@@ -0,0 +1,19 @@
+// # 1 
+// for(let terminal = 1; terminal < 50; terminal++){
+//     if(terminal % 2 === 0){
+//     console.log("terminal")
+//     }
+// }
+
+// # 2
+// for(let terminal = 1; terminal < 200; terminal++){
+//     if(terminal % 2 === 0){
+//         console.log('Even: ' + terminal)
+//     } else {
+//         console.log('odd: ' + terminal)
+//     }
+// }
+
+// for(let GOA = 0; GOA = 3; GOA++){
+//     console.log("GOA IS THE BEST")
+// }
