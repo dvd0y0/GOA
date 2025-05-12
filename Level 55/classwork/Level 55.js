dcf8aec8e3490c9c0327e45af92058fe4de00c7b
dvd0y0 <dvdmetskhovrishvili@gmail.com>
2025-05-12 23:46:08 +0400

    commit

---
+++ Level 55/classwork/Level 55.js
@@ -0,0 +1,18 @@
+// # 1
+// let name = prompt('name: ')
+
+// function name(name){
+//     console.log(`welcome ${name}`);
+// }
+
+// name(name);
+
+// # 2
+// let feet = prompt('Enter feet value: ');
+
+// function convertToInches(f) {
+//     let inches = f * 12;
+//     console.log(`${f} foot has ${inches} inches`);
+// }
+
+// convertToInches(feet);
