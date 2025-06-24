16be5cd63c059955c85a694acfdeee5f593a9bc6
dvd0y0 <dvdmetskhovrishvili@gmail.com>
2025-06-24 15:31:28 +0400

    commit

---
+++ Level 61/classwork/Level 61.js
@@ -0,0 +1,15 @@
+let name = ['dato','vano','ilia','saba','saba'];
+
+let names = name[0]
+let names1 = name[1]
+let names2 = name[2]
+let names3 = name[3]
+let names4 = name[4]
+let names5 = name[5]
+
+console.log(names)
+console.log(names1)
+console.log(names2)
+console.log(names3)
+console.log(names4)
+console.log(names5)
---
+++ Level 61/homework/Level 61.js
@@ -0,0 +1,63 @@
+// #1 
+// function countElements(arr) {
+//   console.log("Array length:", arr.length);
+
+// }
+
+// #2 
+// function concatenateArrays(arr1, arr2) {
+//   const result = arr1.concat(arr2);
+//   console.log("Concatenated array:", result);
+// }
+
+// #3 
+// function addToEnd(arr, value) {
+//   arr.push(value);
+//   console.log("Array after push:", arr);
+//   console.log("New length:", arr.length);
+// }
+
+// #4
+// function removeLastElement(arr) {
+//   const removed = arr.pop();
+//   console.log("Removed element:", removed);
+//   console.log("Array after pop:", arr);
+// }
+
+// #5
+// function concatAndPush(arr1, arr2, value) {
+//   const newArray = arr1.concat(arr2);
+//   console.log("Concatenated array:", newArray);
+//   newArray.push(value);
+//   console.log("Array after push:", newArray);
+// }
+
+// #6
+// function sumArray(arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     console.log(`Adding element at index ${i}: ${arr[i]}`);
+//     sum += arr[i];
+//   }
+//   console.log("Total sum:", sum);
+// }
+
+// #7
+// function evenIndexedArray(arr) {
+//   const newArr = [];
+//   for (let i = 0; i < arr.length; i += 2) {
+//     console.log(`Adding element at even index ${i}: ${arr[i]}`);
+//     newArr.push(arr[i]);
+//   }
+//   console.log("Even-indexed array:", newArr);
+// }
+
+// #8
+// function stringLengths(arr) {
+//   const lengths = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     console.log(`Length of string "${arr[i]}" at index ${i}: ${arr[i].length}`);
+//     lengths.push(arr[i].length);
+//   }
+//   console.log("Array of string lengths:", lengths);
+// }
