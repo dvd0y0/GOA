01ea051a2b9a9dcaf9fa1da6af8b26070ad3c939
dvd0y0 <dvdmetskhovrishvili@gmail.com>
2025-07-31 17:04:12 +0400

    commit

---
+++ Level 67/classwork/Level 67.js
@@ -0,0 +1,20 @@
+// # 1
+// const paragraphs = document.getElementsByClassName('first-group');
+// for(let i = 0; i < paragraphs.length; i++) {
+//     paragraphs.textContent = "class p tag";
+// }
+
+// const p6 = document.getElementById('p6');
+// p6.textContent = "Unique text for p6";
+
+// const p7 = document.getElementById('p7');
+// p7.textContent = "Unique text for p7";
+
+// const p8 = document.getElementById('p8');
+// p8.textContent = "Unique text for p8";
+
+// const p9 = document.getElementById('p9');
+// p9.textContent = "Unique text for p9";
+
+// const p10 = document.getElementById('p10');
+// p10.textContent = "Unique text for p10";
---
+++ Level 67/homework/Level 67.js
@@ -0,0 +1,35 @@
+// # 1 
+
+// სურათის მანიპულირება
+// const imgElement = document.querySelector("#img");
+// imgElement.src = "image.png";
+// imgElement.style.border = "5px solid red";
+
+// ელემენტის მოძიება და ლოგირება
+// const p = document.getElementById("p");
+// console.log(p[0]);
+
+// innerHTML გამოყენება
+// const div = document.getElementById("parent");
+// div.innerHTML = "hello world";
+// console.log(div);
+
+// დინამიური შიგთავსის დამატება
+// const divDynamic = document.querySelector("div");
+// let user = prompt("Enter something");
+// divDynamic.innerHTML = `<button>${user}</button>`;
+
+// ჩასმული HTML
+// const parentDiv = document.querySelector(".parent");
+// parentDiv.innerHTML = "<div><button>Hello</button></div>";
+
+// ბრაუზერის გამოყენება
+// const p3 = document.getElementById("p3");
+// p3.textContent = "hello";
+
+// # 2
+
+// const element = document.getElementById("Id");  აბრუნებს ელემენტს
+// const elements = document.getElementsByClassName("Class");  აბრუნებს HTMLCollection
+// console.log(element);  ელემენტის ობიექტი
+// console.log(elements[0]);  პირველი ელემენტი კოლექციიდან
