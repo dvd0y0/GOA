dd1aec72dc7fab989bc8c9b4d7fa1b0a6eabba9e
dvd0y0 <dvdmetskhovrishvili@gmail.com>
2025-09-07 12:48:12 +0400

    commit

---
+++ Level 73/classwork/Level 73.js
@@ -0,0 +1,108 @@
+// # 1
+
+// const dato = ["davit", "14", "vcxovrob"];
+
+// const gio = ["gio", "14", "vcxovrob var"];
+
+// const zaza = ["zaza", "14", "vcxovrob"];
+
+// const lasha = ["lasha", "14", "aris"];
+
+// const lizi = ["lizi", "14", "aq aris"];
+ // -----------------------------------
+// const dato1 = {
+//     name: "dato",
+//     age: "14",
+//     live: "vcxovrob"
+// }
+
+// const gio1 = {
+//     name: "gio",
+//     age: "14",
+//     live: "vcxovrob var"
+// }
+
+// const zaza1 = {
+//     name: "zaza",
+//     age: "14",
+//     live: "vcxovrob"
+// }
+
+// const lasha1 = {
+//     name: "lasha",
+//     age: "14",
+//     live: "aris"
+// }
+
+// const lizi1 = {
+//     name: "lizi",
+//     age: "14",
+//     live: "aq aris"
+// }
+ // -----------------------------------
+
+// for (let a of dato){
+//     console.log(a)
+// }
+
+// for (let a of gio){
+//     console.log(a)
+// }
+
+// for (let a of zaza){
+//     console.log(a)
+// }
+
+// for (let a of lasha){
+//     console.log(a)
+// }
+
+// for (let a of lizi){
+//     console.log(a)
+// }
+
+ // -----------------------------------
+
+// for (let a in dato1){
+//     console.log(a, dato1[a])
+// }
+
+// for (let a in gio1){
+//     console.log(a, gio1[a])
+// }
+
+// for (let a in zaza1){
+//     console.log(a, zaza1[a])
+// }
+
+// for (let a in lasha1){
+//     console.log(a, lasha1[a])
+// }
+
+// for (let a in lizi1){
+//     console.log(a, lizi1[a])
+// }
+
+// # 2
+
+// const Hello = () => console.log("hello world");
+// Hello();
+
+//  // -----------------------------------
+
+// const add = (a, b) => {
+//   const num = a + b;
+//   console.log(num);
+//   return num;
+// };
+// add(5, 3);
+
+// # 3
+
+// const dato = ["davit", "14", "vcxovrob"];
+
+// dato.forEach((item, index) => {
+//     console.log("start");
+//     console.log(index, item);
+//     console.log("end");
+// });
---
+++ Level 73/homework/Level 73.js
@@ -0,0 +1,59 @@
+// # 1
+// const myArray = ["apple", "banana", "orange", "grape", "mango"];
+
+// const myObject = {
+//   name: "John",
+//   age: 25,
+//   city: "Tbilisi"
+// };
+
+// console.log("Array elements using for...of:");
+// for (let item of myArray) {
+//   console.log(item);
+// }
+
+// console.log("Object properties using for...in:");
+// for (let key in myObject) {
+//   console.log(`${key}: ${myObject[key]}`);
+// }
+
+// # 2
+
+// const Hello = () => "Hello World";
+// console.log(Hello());
+
+// const add = (a, b) => {
+//   const sum = a + b;
+//   console.log(sum);
+//   return sum;
+// };
+// add(5, 3);
+
+// # 3
+
+
+// const myArray = ["apple", "banana", "orange", "grape", "mango"];
+
+// myArray.forEach((item, index) => {
+//   console.log(`Index: ${index}, Item: ${item}`);
+// });
+
+// # 4
+
+// const myObject = {
+//   name: "John",
+//   age: 25,
+//   city: "Tbilisi"
+// };
+
+// for (let key in myObject) {
+//   console.log(`Key: ${key}, Value: ${myObject[key]}`);
+// }
+
+// # 5 
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// const doubled = numbers.map(num => num * 2);
+
+// console.log(doubled);
