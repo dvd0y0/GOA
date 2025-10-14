aa4566882db97868532a04afaeadf44fdaa6b5ad
dvd0y0 <dvdmetskhovrishvili@gmail.com>
2025-10-14 14:31:09 +0400

    commit

---
+++ Level 77/classwork/Level 77.js
@@ -0,0 +1,19 @@
+// # 1
+// function Morning(){
+//     return Morning
+// }
+
+// function Afternoon(){
+//     return Afternoon
+// }
+
+// function Night(){
+// setTimeout( () => {
+//         return Night
+//     }, 1000);
+// }
+
+// console.log(Morning())
+// console.log(Afternoon())
+// console.log(Night())
+
