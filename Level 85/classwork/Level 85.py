0dd3c493e3755e6431c9ddc1e306b0941861e7b3
dvd0y0 <dvdmetskhovrishvili@gmail.com>
2025-12-06 19:06:02 +0400

    commit

---
+++ Level 85/classwork/Level 85.py
@@ -0,0 +1,39 @@
+# 1
+# class A:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+#     def __str__(self):
+#         return f"name: {self.name}, age: {self.age} "
+
+#     def greeting(self):
+#         return f"hi {self.name} are you {self.age}?"
+
+# AA = A("datuna", "ramdenime")
+
+# print(AA.greeting())
+
+# 2
+# class A:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+#     def __str__(self):
+#         return f"name: {self.name}, age: {self.age} "
+
+#     def greeting(self):
+#         return f"hi {self.name} are you {self.age}?"
+
+
+# class AAB(A):
+#     def __init__(self, klichka, metsaxeli):
+#         A.__init__(self, klichka, metsaxeli)
+
+
+# AABB = AAB("bubu", "zuzu")
+
+# print(AABB.greeting())
+
+# 3
